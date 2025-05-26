4f1c747460f325fe78f8ec5e8cc27674f73da419
Jean-Baptiste THERY <jb.thery@jcode.works>
2025-05-26 15:03:45 +0200

    feat: project add dev tools
    Signed-off-by: Jean-Baptiste THERY <jb.thery@jcode.works>

---
+++ pwa-assets.config.js
@@ -0,0 +1,9 @@
+import { defineConfig, minimal2023Preset as preset } from '@vite-pwa/assets-generator/config';
+
+export default defineConfig({
+	headLinkOptions: {
+		preset: 'minimal-2023'
+	},
+	preset,
+	images: ['static/logo-khartis.svg']
+});
