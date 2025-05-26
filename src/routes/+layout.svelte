48d30b87886f5325cad297ad3f340248e168b8a3
Jean-Baptiste THERY <jb.thery@jcode.works>
2025-05-26 14:08:08 +0200

    feat: implement spa mode and theme switch
    Signed-off-by: Jean-Baptiste THERY <jb.thery@jcode.works>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
-	import 'carbon-components-svelte/css/white.css';
+	import { Theme } from 'carbon-components-svelte';
+	import type { CarbonTheme } from 'carbon-components-svelte/src/Theme/Theme.svelte';
+
+	import 'carbon-components-svelte/css/all.css';
+
 	import '../app.css';
 
 	let { children } = $props();
+
+	let theme: CarbonTheme = $state('white');
 </script>
+
+<Theme bind:theme />
 
 {@render children()}
