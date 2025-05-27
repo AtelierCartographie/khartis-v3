6707d009afdd9b984aa54907b7a508b461bc96d3
Jean-Baptiste THERY <jb.thery@jcode.works>
2025-05-27 12:48:26 +0200

    feat: add base wasm duck db integration
    Signed-off-by: Jean-Baptiste THERY <jb.thery@jcode.works>

---
+++ src/lib/db/index.ts
@@ -0,0 +1,11 @@
+export {
+	dbError,
+	duckDB,
+	duckDbState,
+	isDbInitializing,
+	isDbReady,
+	isQuerying,
+	queryError
+} from './client';
+
+export type { DuckDBState, QueryState } from './client';
