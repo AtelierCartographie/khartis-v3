5002c48aa2318740d1cfd7e230b47235839cab58
Jean-Baptiste THERY <jb.thery@jcode.works>
2025-05-27 12:48:26 +0200

    feat: add base wasm duck db integration
    Signed-off-by: Jean-Baptiste THERY <jb.thery@jcode.works>

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,14 @@
-import type { Handle } from '@sveltejs/kit';
 import { paraglideMiddleware } from '$lib/paraglide/server';
+import type { Handle } from '@sveltejs/kit';
+import { sequence } from '@sveltejs/kit/hooks';
+
+const handleDevTools: Handle = ({ event, resolve }) => {
+	if (event.url.pathname.startsWith('/.well-known/')) {
+		return new Response('Not Found', { status: 404 });
+	}
+
+	return resolve(event);
+};
 
 const handleParaglide: Handle = ({ event, resolve }) =>
 	paraglideMiddleware(event.request, ({ request, locale }) => {
@@ -10,4 +19,4 @@ const handleParaglide: Handle = ({ event, resolve }) =>
 		});
 	});
 
-export const handle: Handle = handleParaglide;
+export const handle: Handle = sequence(handleDevTools, handleParaglide);
