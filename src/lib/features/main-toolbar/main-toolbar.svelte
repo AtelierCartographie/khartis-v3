b7ded93c7026d54fdd5fe3531e022b67086b978a
Jean-Baptiste THERY <jb.thery@jcode.works>
2025-12-04 14:37:45 +0800

    fix: data tab design issues, and clean project
    Signed-off-by: Jean-Baptiste THERY <jb.thery@jcode.works>

--- src/lib/features/main-toolbar/main-toolbar.svelte
+++ src/lib/features/main-toolbar/main-toolbar.svelte
@@ -140,7 +140,7 @@
 
     <footer
       class={clsx(
-        'sticky bottom-0  bg-white p-5 border-t',
+        'sticky bottom-0 bg-white p-5 border-t z-1000',
         globalState.toolbarState === ToolbarState.Collapsed && 'opacity-0'
       )}
     >
