6ea21eb1c37883149cc531d1dbd6910868123bea
Jean-Baptiste THERY <jb.thery@jcode.works>
2025-12-04 17:56:16 +0800

    chore: remove useless step on project creation
    Signed-off-by: Jean-Baptiste THERY <jb.thery@jcode.works>

--- src/lib/features/commons/store/global.svelte.ts
+++ src/lib/features/commons/store/global.svelte.ts
@@ -21,7 +21,6 @@ class GlobalStore {
     mainPanel: true,
     isSideNavOpen: false,
     isCreateProjectModalOpen: false,
-    isDataTypeSelectionModalOpen: false,
     selectedStep: ToolbarStep.Data,
     selectedTool: undefined,
     toolbarState: ToolbarState.Full,
@@ -192,14 +191,6 @@ class GlobalStore {
 
   set isCreateProjectModalOpen(value: boolean) {
     this._state.isCreateProjectModalOpen = value;
-  }
-
-  get isDataTypeSelectionModalOpen() {
-    return this._state.isDataTypeSelectionModalOpen;
-  }
-
-  set isDataTypeSelectionModalOpen(value: boolean) {
-    this._state.isDataTypeSelectionModalOpen = value;
   }
 
   get selectedStep() {
--- src/lib/features/main-toolbar/components/add-data-modal.svelte
+++ src/lib/features/main-toolbar/components/add-data-modal.svelte
@@ -3,15 +3,8 @@
     createProjectActions,
     createProjectState
   } from '$lib/features/commons/store/create-project.store.svelte';
-  import {
-    globalActions,
-    globalState
-  } from '$lib/features/commons/store/global.svelte';
+  import { globalActions } from '$lib/features/commons/store/global.svelte';
   import { projectStore } from '$lib/features/commons/store/project.store.svelte';
-  import {
-    dataTypeSelectionActions,
-    dataTypeSelectionStore
-  } from '$lib/features/commons/components/data-type-selection';
   import { LogCategory, logger } from '$lib/features/commons/utils/logger';
   import CreateNewProject from '$lib/features/create-project/create-new-project.svelte';
   import { Modal } from 'carbon-components-svelte';
@@ -65,14 +58,6 @@
         const lastFile = validFiles[validFiles.length - 1];
         if (lastFile?.id) {
           globalActions.selectDataButton(lastFile.id);
-        }
-
-        // Check if we need to show data type selection modal
-        const shouldShowDataTypeModal =
-          dataTypeSelectionStore.shouldShowModal(validFiles);
-        if (shouldShowDataTypeModal) {
-          dataTypeSelectionActions.initializeFromFiles(validFiles);
-          globalState.isDataTypeSelectionModalOpen = true;
         }
 
         closeModal();
